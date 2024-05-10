d5c93810fabe079506a393b766627ea678f160ad
Lukasz Gruszka <lukasz>
2024-05-10 14:44:58 +0200

    Add basic configuration

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2023': true,
+  },
+  'extends': 'google',
+  'overrides': [
+    {
+      'env': {
+        'node': true,
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+      },
+    },
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+    'jsdoc/no-undefined-types': [
+      1,
+      {
+        'definedTypes': [
+          'chrome', 'RequestInit',
+        ],
+      },
+    ],
+    'max-len': 'off',
+    'require-jsdoc': 'error',
+  },
+  'plugins': [
+    'jsdoc',
+  ],
+};
