e211b6bf8d5d5b9668c7c1061a649e03c17b0ed5
Lukasz Gruszka <lukasz>
2024-10-03 18:07:39 +0200

    copy oauth and scheduler livraries

---
+++ js/oauth.js
@@ -0,0 +1,353 @@
+const oauthStateParam = 'state';
+const oauthCodeParam = 'code';
+
+/**
+ * @typedef {Object} AccessToken
+ * @property {string} code
+ * @property {string} client_id
+ * @property {string} client_secret
+ * @property {URL} redirect_uri
+ * @property {'authorization_code'} grant_type
+ */
+
+/**
+ * @typedef {Object} RefreshToken
+ * @property {string} client_id
+ * @property {string} client_secret
+ * @property {string} refresh_token
+ * @property {'refresh_token'} grant_type
+ */
+
+/**
+ * Helper method that should be invoked in the oauth redirect window.
+ * The popup page needs to specify OAuthSettings name in 'state' get parameter.
+ * This method will:
+ *   - load the OAuthSettings
+ *   - parse the returned oauth code from the url
+ *   - request refresh token and save it in OAuthSettings
+ *   - close the popup window and reload its parent
+ *
+ * @return {Promise<void>}
+ */
+export function processOAuthRedirect() {
+  const url = new URL(window.location.href);
+  const name = url.searchParams.get(oauthStateParam);
+  if (!name) {
+    return Promise.reject(new Error(`Url missing param: ${oauthStateParam}`));
+  }
+
+  const s = new OAuthSettings(name);
+  if (!s.isInitialised()) {
+    return Promise.reject(new Error(`Settings ${s} should be initialised in processOAuthRedirect`));
+  }
+
+  const code = url.searchParams.get(oauthCodeParam);
+  if (!code) {
+    return Promise.reject(new Error(`Url missing param: ${oauthCodeParam}`));
+  }
+
+  /** @type {RequestInit} */
+  const params = {method: 'POST', mode: 'cors', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(s.createRefreshTokenData(code))};
+
+  return fetch(s.getTokenUrl(), params)
+      .then((response) => {
+        if (response.status != 200) {
+          throw new Error(`Cannot get token: url: ${s.getTokenUrl()}, response: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((json) => {
+        if (!json.refresh_token) {
+          throw new Error(`RefreshToken not found in response: ${json}`);
+        }
+        s.setRefreshToken(json.refresh_token);
+        s.save();
+        window.opener.location.reload();
+        window.close();
+        return;
+      });
+}
+
+/**
+ * OAuth class that will take care of the OAuth flow.
+ * If OAuthSettings are properly configured, it will return
+ * the token after calling getAccessToken() method.
+ */
+export class OAuth {
+  #settings;
+  #accessToken;
+
+  /**
+   * @param {OAuthSettings} settings
+   */
+  constructor(settings) {
+    console.log('OAuth created');
+    this.#settings = settings;
+  }
+
+  /**
+   * @param {boolean} forceRefresh
+   * @return {Promise<string>}
+   */
+  getAccessToken(forceRefresh = false) {
+    if (forceRefresh) {
+      this.#accessToken = undefined;
+    }
+    if (this.#accessToken) {
+      return Promise.resolve(this.#accessToken);
+    }
+
+    if (!this.#settings.hasRefreshToken()) {
+      this.#openOAuthPopup();
+      // Page will be refreshed and token will be requested again.
+      return Promise.reject(new Error('No refresh token in settings'));
+    }
+
+    const accessTokenData = this.#settings.createAccessTokenData();
+    if (!accessTokenData) {
+      return Promise.reject(new Error(`Settings not initialised: ${this.#settings}`));
+    }
+
+    /** @type {RequestInit} */
+    const params = {method: 'POST', mode: 'cors', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(accessTokenData)};
+    return fetch(this.#settings.getTokenUrl(), params)
+        .then((response) => (response.status == 200) ? response :
+           // @ts-ignore
+           /** @type {Response} */(this.#throwError(`Cannot get access token: url: ${this.#settings.getTokenUrl()}, response: ${response.status}`, true)))
+        .then((response) => response.json())
+        .then((json) => json.access_token ? (this.#accessToken = json.access_token) : this.#throwError(`AccessToken not found in response: ${json}`, false));
+  }
+
+  /**
+   * @param {string} message
+   * @param {boolean} openOAuthPopup
+   */
+  #throwError(message, openOAuthPopup) {
+    console.error(message);
+    console.error('Removing RefreshToken.');
+    this.#settings.setRefreshToken('');
+    this.#settings.save();
+    if (openOAuthPopup) {
+      this.#openOAuthPopup();
+    }
+    throw new Error(message);
+  }
+
+  /**
+   * Opens the popup window to get code
+   */
+  #openOAuthPopup() {
+    const url = this.#settings.createOAuthUrl();
+    if (url) {
+      window.open(url, 'window', 'toolbar=no, menubar=no, resizable=yes');
+    } else {
+      console.error(`Invalid url. Settings: ${this.#settings}`);
+    }
+  }
+}
+
+/**
+ * Class that contains OAuthSettings.
+ * It will load and save the settings from the local storage under the specified #name.
+ * To see which parameters are required please check isInitialised() method.
+ */
+export class OAuthSettings {
+  static #type = 'OAuthSettings';
+  #name;
+  #data;
+
+  /**
+   * @param {string} name
+   */
+  constructor(name) {
+    this.#name = name;
+    this.#data = JSON.parse(localStorage.getItem(name) || 'null') || {type: OAuthSettings.#type};
+    if (OAuthSettings.#type == this.#data.type) {
+      console.log('OAuthSettings created: ' + this);
+    } else {
+      console.warn('OAuthSettings created [saved data contains invalid type]: ' + this);
+    }
+  }
+
+  /**
+   * @return {URL|undefined}
+   */
+  createOAuthUrl() {
+    if (!this.isInitialised()) {
+      return undefined;
+    }
+
+    const redirectUrl = new URL(this.#data.redirectUrl);
+
+    const oAuthUrl = new URL(this.#data.oAuthUrl);
+    oAuthUrl.searchParams.set('client_id', this.#data.clientId);
+    oAuthUrl.searchParams.set(oauthStateParam, this.#name);
+    oAuthUrl.searchParams.set('redirect_uri', redirectUrl.toString());
+    oAuthUrl.searchParams.set('scope', this.#data.scope);
+    oAuthUrl.searchParams.set('response_type', 'code');
+    oAuthUrl.searchParams.set('access_type', 'offline');
+    oAuthUrl.searchParams.set('approval_prompt', 'force');
+    return oAuthUrl;
+  }
+
+  /**
+   * @param {string} code
+   * @return {AccessToken|undefined}
+   */
+  createRefreshTokenData(code) {
+    if (!this.isInitialised()) {
+      return undefined;
+    }
+
+    return {
+      code: code,
+      client_id: this.#data.clientId,
+      client_secret: this.#data.clientSecret,
+      redirect_uri: this.#data.redirectUrl,
+      grant_type: 'authorization_code',
+    };
+  }
+
+  /**
+   * @return {RefreshToken|undefined}
+   */
+  createAccessTokenData() {
+    if (!this.isInitialised() || !this.#data.refreshToken) {
+      return undefined;
+    }
+
+    return {
+      client_id: this.#data.clientId,
+      client_secret: this.#data.clientSecret,
+      refresh_token: this.#data.refreshToken,
+      grant_type: 'refresh_token',
+    };
+  }
+
+  /**
+   * Save to storage
+   */
+  save() {
+    localStorage.setItem(this.#name, JSON.stringify(this.#data));
+    console.log('OAuthSettings saved: ' + this);
+  }
+
+  /**
+   * @return {string}
+   */
+  getName() {
+    return this.#name;
+  }
+
+  /**
+   * @return {string}
+   */
+  getTokenUrl() {
+    return this.#data.tokenUrl;
+  }
+
+  /**
+   * @return {string}
+   */
+  getClientId() {
+    return this.#data.clientId;
+  }
+
+  /**
+   * @return {string}
+   */
+  getClientSecret() {
+    return this.#data.clientSecret;
+  }
+
+  /**
+   * @return {boolean}
+   */
+  hasRefreshToken() {
+    return true && this.#data.refreshToken;
+  }
+
+
+  /**
+   * Sets client id (e.g. from Google Developer Console)
+   *
+   * @param {string} clientId
+   */
+  setClientId(clientId) {
+    this.#data.clientId = clientId;
+  }
+
+  /**
+   * Sets client secret (e.g. from Google Developer Console)
+   *
+   * @param {string} clientSecret
+   */
+  setClientSecret(clientSecret) {
+    this.#data.clientSecret = clientSecret;
+  }
+
+  /**
+   * Sets requested scope
+   *
+   * @param {string} scope
+   */
+  setScope(scope) {
+    this.#data.scope = scope;
+  }
+
+  /**
+   * Sets url to obtain initial refresh token, e.g.: https://accounts.google.com/o/oauth2/auth
+   *
+   * @param {string} oAuthUrl
+   */
+  setOAuthUrl(oAuthUrl) {
+    this.#data.oAuthUrl = oAuthUrl;
+  }
+
+  /**
+   * Sets url to refresh token, e.g.: https://accounts.google.com/o/oauth2/token
+   *
+   * @param {string} tokenUrl
+   */
+  setTokenUrl(tokenUrl) {
+    this.#data.tokenUrl = tokenUrl;
+  }
+
+  /**
+   * Sets redirect url - usually url of the page - for this page use https://birnenlabs.com/oauth/popup.html
+   *
+   * @param {string} redirectUrl
+   */
+  setRedirectUrl(redirectUrl) {
+    this.#data.redirectUrl = redirectUrl;
+  }
+
+  /**
+   * Sets refresh token for updating access token
+   *
+   * @param {string} refreshToken
+   */
+  setRefreshToken(refreshToken) {
+    this.#data.refreshToken = refreshToken;
+  }
+
+
+  /**
+   * @return {boolean}
+   */
+  isInitialised() {
+    return this.#data.clientId &&
+        this.#data.clientSecret &&
+        this.#data.scope &&
+        this.#data.oAuthUrl &&
+        this.#data.tokenUrl &&
+        this.#data.redirectUrl;
+  }
+
+  /**
+   * @return {string}
+   */
+  toString() {
+    return `OAuthSettings[name=${this.#name}, data=[clientId=${this.#data.clientId}, clientSecret=${this.#data.clientSecret}, type=${this.#data.type}, scope=${this.#data.scope}, oAuthUrl=${this.#data.oAuthUrl}, tokenUrl=${this.#data.tokenUrl}, redirectUrl=${this.#data.redirectUrl}, refreshToken=${this.#data.refreshToken}]]`;
+  }
+}
---
+++ js/scheduler.js
@@ -0,0 +1,355 @@
+import {checkNonUndefined} from '/jslib/js/preconditions.js';
+
+// Some time in year 2096
+export const EPOCH_FUTURE = 4000000000;
+
+// Base value for exponential backoff.
+const RETRY_DELAY_BASE_SEC = 5;
+
+// Maximum value of retry delay
+const RETRY_DELAY_MAX_SEC = 3600;
+
+/** Base event class */
+class BaseEvent {
+  /** @type {function():any} */
+  #fn;
+
+  /** @type {string} */
+  #id;
+
+  /**
+   * @param {string} id
+   * @param {function():any} fn
+   */
+  constructor(id, fn) {
+    this.#fn = fn;
+    this.#id = id;
+  }
+
+  /**
+   * @return {string}
+   */
+  toString() {
+    return `${this.#id}: fn="${this.#fn.name}"`;
+  }
+
+  /**
+   * @return {string}
+   */
+  id() {
+    return this.#id;
+  }
+
+  /**
+   * Performs function
+   * @return {Promise<any>}
+   */
+  tick() {
+    // Use `Promise.resolve()` to start promise chain even for non promise functions.
+    return Promise.resolve(this.#fn())
+        .catch((e) => {
+          // Log and rethrow failed task.
+          console.error(`${event} failed`, e);
+          throw e;
+        });
+  }
+}
+
+/** Scheduled event class */
+class ScheduledEvent extends BaseEvent {
+  /** @type {number} */
+  #nextRunEpochSec;
+
+  /** @type {number} */
+  #retryDelaySec;
+
+  /**
+   * @param {string} id
+   * @param {function():any} fn
+   * @param {number} nextRunEpochSec
+   */
+  constructor(id, fn, nextRunEpochSec) {
+    super(id, fn);
+    this.#nextRunEpochSec = nextRunEpochSec;
+    this.#retryDelaySec = RETRY_DELAY_BASE_SEC;
+  }
+
+  /**
+   * @return {string}
+   */
+  toString() {
+    // Using sv (Sweden) as it uses iso time format
+    return super.toString() + `, scheduled=${this.isScheduled() ? new Date(this.#nextRunEpochSec * 1000).toLocaleString('sv') : 'never'}`;
+  }
+
+  /**
+   * @return {boolean}
+   */
+  isScheduled() {
+    return this.nextRunEpochSec() !== -1;
+  }
+
+  /**
+   * @return {number}
+   */
+  nextRunEpochSec() {
+    return this.#nextRunEpochSec;
+  }
+
+  /** Will overwrite #nextRunEpochSec to retry the task in #retryDelaySec seconds or earlier */
+  scheduleForRetry() {
+    const retryRunEpochSec = getNowSec() + this.#retryDelaySec;
+    if (this.isScheduled()) {
+      // Reschedule scheduled event only when retry is earlier than expected run.
+      this.setNextRunEpochSec(Math.min(retryRunEpochSec, this.nextRunEpochSec()));
+    } else {
+      this.setNextRunEpochSec(retryRunEpochSec);
+    }
+
+    this.#retryDelaySec = Math.min(this.#retryDelaySec * 2, RETRY_DELAY_MAX_SEC);
+    console.log(`${this}: rescheduled`);
+  }
+
+  /**
+   * @param {number} nextRunEpochSec
+   */
+  setNextRunEpochSec(nextRunEpochSec) {
+    this.#nextRunEpochSec = nextRunEpochSec;
+  }
+
+
+  /**
+   * Performs function
+   * @return {Promise<any>}
+   */
+  tick() {
+    return super.tick()
+        // Reset retry timer on success
+        .then(() => this.#retryDelaySec = RETRY_DELAY_BASE_SEC)
+        // Always set next run
+        .finally(() => this.setNextRunEpochSec(-1));
+  }
+
+  /**
+   * @param {ScheduledEvent} e1
+   * @param {ScheduledEvent} e2
+   * @return {number}
+   */
+  static compare(e1, e2) {
+    return (e1.#nextRunEpochSec - e2.#nextRunEpochSec) || e1.id().localeCompare(e2.id());
+  }
+}
+
+/** Repeatable event class */
+class RepeatableEvent extends ScheduledEvent {
+  /** @type {number} */
+  #intervalSec;
+
+  /**
+   * @param {string} id
+   * @param {function():any} fn
+   * @param {number} intervalSec
+   */
+  constructor(id, fn, intervalSec) {
+    super(id, fn, RepeatableEvent.#calculateNextRunEpochSec(intervalSec));
+    this.#intervalSec = intervalSec;
+  }
+
+  /**
+   * @return {string}
+   */
+  toString() {
+    return super.toString() + `, repeatable every ${this.#intervalSec/60}m`;
+  }
+
+  /**
+   * Perform function
+   * @return {Promise<any>}
+   */
+  tick() {
+    return super.tick()
+        // Always set the next run.
+        .finally(() => super.setNextRunEpochSec(RepeatableEvent.#calculateNextRunEpochSec(this.#intervalSec)));
+  }
+
+  /**
+   * Will return next epoch seconds timestamp when event should be run.
+   *
+   * @param {number} intervalSec
+   * @return {number}
+   */
+  static #calculateNextRunEpochSec(intervalSec) {
+    const offsetSec = new Date().getTimezoneOffset() * 60;
+    const nowSec = getNowSec();
+    const result = nowSec + intervalSec - ((nowSec - offsetSec) % intervalSec);
+    return result;
+  }
+}
+
+/**
+ * Class that runs the specified function periodically.
+ *
+ * SetTimeout bahaviour depends on implementation - it may happen
+ * that it will pause the timer while sleeping (i.e. when setTimout
+ * for 1h is set and the computer is paused for 30 minutes the function
+ * may be invoked after 90 minutes).
+ * This implementation ticks every second and stores the next invocation
+ * timestamp.
+ */
+class Scheduler {
+  // when setting script to run every 24 hours it is expected to
+  // run at midnight local time - need to adjust epoch to be in
+  // local time
+  static #tzOffsetSec = new Date().getTimezoneOffset() * 60;
+
+  /** @type {BaseEvent[]} */
+  static #everyTickEvents = [];
+
+  /** @type {ScheduledEvent[]} */
+  static #scheduledEvents = [
+    // Adding last event to guard the end of an array.
+    // There will be no need to check array length on #tick.
+    new ScheduledEvent('__?# internal Scheduler last event #?__', ()=>{}, EPOCH_FUTURE),
+  ];
+
+  static {
+    setTimeout(Scheduler.#tick, 1000 - (Date.now() % 1000));
+  }
+
+  /**
+   * Function invoked every second.
+   * @return {Promise<any>}
+   */
+  static #tick() {
+    const nowSec = getNowSec();
+
+    /** @type {ScheduledEvent[]} */
+    const eventsToRun = [];
+
+    // Scheduled events are always sorted and at least one element exists.
+    while (Scheduler.#scheduledEvents[0].nextRunEpochSec() <= nowSec) {
+      eventsToRun.push(checkNonUndefined(Scheduler.#scheduledEvents.shift()));
+    }
+
+    let tickEventsPromise =
+       // Run #everyTickEvents in parallel
+       Promise.all(
+           Scheduler.#everyTickEvents
+               // Swallow exception here - failed are already logged and #everyTickEvents will be tried at the next tick anyway
+               .map((event) => event.tick().catch((e) => {})))
+           // Schedule the next tick in 1 second after all the #everyTickEvents completed.
+           .then(() => setTimeout(Scheduler.#tick, 1000 - (Date.now() % 1000)));
+
+    // Run the remaining eventsToRun in the sequence
+    for (const event of eventsToRun) {
+      // Failed non #everyTickEvents should be retried
+      tickEventsPromise = tickEventsPromise.then(() => event.tick().catch((e) => event.scheduleForRetry()));
+    }
+
+    return tickEventsPromise
+        // Maybe re-add functions from eventsToRun (including events scheduled for retry).
+        .then(() => eventsToRun.forEach((event) => event.isScheduled() && Scheduler.add(event)))
+        .catch((e) => console.error(`Unexpected exception in scheduler - this is a bug.`, e));
+  }
+
+  /**
+   * @param {BaseEvent|ScheduledEvent|RepeatableEvent} event
+   */
+  static add(event) {
+    const findSameIdFn = (e) => e.id() === event.id();
+
+    // RepeatableEvent is ScheduledEvent
+    if (event instanceof ScheduledEvent) {
+      if (!event.isScheduled()) {
+        console.error(`Scheduler: cannot add non scheduled event: ${event}`);
+      }
+
+      // Check if event with the same id exists in the scheduler array.
+      const index = Scheduler.#scheduledEvents.findIndex(findSameIdFn);
+      if (index == -1) {
+        console.log(`Scheduler add: ${event}`);
+        Scheduler.#scheduledEvents.push(event);
+      } else {
+        console.log(`Scheduler replace: ${Scheduler.#scheduledEvents[index]} with ${event}`);
+        Scheduler.#scheduledEvents[index] = event;
+      }
+      Scheduler.#scheduledEvents.sort(ScheduledEvent.compare);
+    } else if (event instanceof BaseEvent) {
+      // Check if event with the same id exists in the every second events array.
+      const index = Scheduler.#everyTickEvents.findIndex(findSameIdFn);
+      if (index == -1) {
+        console.log(`Scheduler add: ${event}, repeatable every 1s`);
+        Scheduler.#everyTickEvents.push(event);
+      } else {
+        console.log(`Scheduler replace: ${Scheduler.#everyTickEvents[index]} with ${event}, repeatable every 1s`);
+        Scheduler.#everyTickEvents[index] = event;
+      }
+    } else {
+      console.error(`Scheduler: Invalid event: ${event}`);
+    }
+  }
+}
+
+
+/** Exported class used to hold static constructors of events */
+export class Event {
+  /**
+   * Repetable events will be called every ${min} minutes or
+   * every second when minute not set.
+   *
+   * @param {string} id
+   * @param {function():any} fn
+   * @param {number} min
+   */
+  static repeat(id, fn, min = 0) {
+    if (min < 0) {
+      throw new Error(`Negative min value: ${min}.`);
+    } else if (min === 0) {
+      Scheduler.add(new BaseEvent(id, fn));
+    } else {
+      Scheduler.add(new RepeatableEvent(id, fn, min * 60));
+    }
+  }
+
+  /**
+   * @param {string} id
+   * @param {function():any} fn
+   * @param {number} min
+   * @param {number} sec
+   */
+  static once(id, fn, min = 0, sec = 0) {
+    Scheduler.add(new ScheduledEvent(id, fn, getNowSec() + min * 60 + sec));
+  }
+
+  /**
+   * @param {string} id
+   * @param {function():any} fn
+   * @param {number} timestampSec
+   */
+  static at(id, fn, timestampSec) {
+    Scheduler.add(new ScheduledEvent(id, fn, timestampSec));
+  }
+}
+
+/**
+ * @return {number}
+ */
+export function getNowSec() {
+  return Math.floor(Date.now() / 1000);
+}
+
+/**
+ * @param {Date} date
+ * @return {number}
+ */
+export function dateToSec(date) {
+  return Math.floor(date.getTime() / 1000);
+}
+
+/**
+ * @param {number} sec
+ * @return {Date}
+ */
+export function secToDate(sec) {
+  return new Date(sec * 1000);
+}
